dd51689d9a55b4cf1bb05f11b6a65e94d3fb8f59
gemusic <gegemusicking@gmail.com>
2026-01-19 10:58:23 +0100

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -10,22 +10,12 @@ interface KkiapayConfig {
   name?: string;
   email?: string;
   phone?: string;
-  data?: string;
 }
 
-declare global {
-  interface Window {
-    kkiapay?: {
-      open: (config: KkiapayConfig) => void;
-      addSuccessListener: (callback: (response: any) => void) => void;
-      addFailedListener: (callback: (error: any) => void) => void;
-    };
-    openKkiapayWidget?: (config: KkiapayConfig) => void;
-    _kkiapayLoaded?: boolean;
-  }
-  
-  // Pour le script global kkiapay
-  const kkiapay: any;
+interface Window {
+  kkiapay?: {
+    open: (config: KkiapayConfig) => void;
+    addSuccessListener: (callback: (response: any) => void) => void;
+    addFailedListener: (callback: (error: any) => void) => void;
+  };
 }
-
-export {};
